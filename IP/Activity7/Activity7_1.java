48a4830d833cfedc0f43b173d4668921107893f7
Prietse <jhonpritse@gmail.com>
2021-05-23 22:33:15 +0800

    Update Activity7_1.java

--- IP/Activity7/Activity7_1.java
+++ IP/Activity7/Activity7_1.java
@@ -1,9 +1,18 @@
 package IP.Activity7;
 
-public class Activity7_1 {
+import java.util.Scanner;
 
+public class Activity7_1 {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        String X;
+        System.out.print("Enter Letter :");
+        X = input.next();
+        switch (Character.toUpperCase(X.charAt(0))) {
+            case 'A', 'E', 'I', 'O', 'U' -> System.out.println("Vowel");
+            default -> System.out.println("Consonant");
+        }
     }
 }
